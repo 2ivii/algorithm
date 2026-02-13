721ae90faa1a584abb225ef95baa3f0dfaeab50b
윤정민 <163484088+2ivii@users.noreply.github.com>
2026-02-13 21:42:24 +0900

    [Bronze IV] Title: 주사위 세개, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,15 @@
+import sys
+
+input=sys.stdin.readline
+
+arr = list(map(int,input().split()))
+
+
+if arr[0]==arr[1] and arr[1]==arr[2]:
+    print(10000+arr[1]*1000)
+elif arr[0]==arr[1] or arr[0]==arr[2]:
+    print(1000+arr[0]*100)
+elif arr[1]==arr[2]:
+    print(1000+arr[1]*100)
+else:
+    print(max(arr)*100)

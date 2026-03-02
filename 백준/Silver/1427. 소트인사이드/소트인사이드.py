0fc58a1545af70f47b5b9031bfdf60bd1368c25d
윤정민 <163484088+2ivii@users.noreply.github.com>
2026-03-02 21:18:56 +0900

    [Silver V] Title: 소트인사이드, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -3,7 +3,16 @@ import sys
 input=sys.stdin.readline
 
 arr=list(input().strip())
-arr.sort(reverse=True)
+l=len(arr)
 
-for i in range(len(arr)):
+for i in range(l):
+    m=i
+    for j in range(i,l):
+        if arr[m]<arr[j]:
+            m=j
+    temp=arr[m]
+    arr[m]=arr[i]
+    arr[i]=temp
+    
+for i in range(l):
     print(arr[i],end='')

438df30ae786084ae16a64e2cdad1ad47b2465f2
윤정민 <163484088+2ivii@users.noreply.github.com>
2026-03-04 22:49:08 +0900

    [Silver II] Title: 섬의 개수, Time: 68 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,38 +1,34 @@
-from sys import stdin
+import sys
 from collections import deque
-input = stdin.readline 
 
-dx = [1, -1, 0, 0, 1, -1, 1, -1] 
-dy = [0, 0, 1, -1, 1, -1, -1, 1]
+input=sys.stdin.readline
 
-def bfs(x, y):
-    que = deque()
-    que.append((x, y))
-    graph[y][x] = 0
-    while que:
-       a, b = que.pop()
-       for i in range(8):
-           ny = b + dy[i]
-           nx = a + dx[i]
-           if 0<=nx<m and 0<=ny<n and graph[ny][nx] == 1: 
-               que.append((nx, ny))
-               graph[ny][nx] = 0
+w,h=map(int,input().split())
+
+dx=[-1,-1,-1,0,0,1,1,1]
+dy=[-1,0,1,-1,1,-1,0,1]
+
+def bfs(i,j,w,h,m):
+    queue=deque([(i,j)])
+    m[i][j] = 0
+    while queue:
+        x,y = queue.popleft()
+        for k in range(8):
+            nx=x+dx[k]
+            ny=y+dy[k]
+            if 0 <= nx < h and 0 <= ny < w and m[nx][ny] == 1 :
+                m[nx][ny] = 0
+                queue.append((nx,ny))
 
 
-result = []
-while True:
-    res = 0
-    m, n = map(int, input().split())
-    if n == 0 and m == 0:
-        break
-    graph = [list(map(int, input().split())) for _ in range(n)]
+while w != 0 and h != 0:
+    m=[list(map(int,input().split())) for _ in range(h)]
+    cnt=0
+    for i in range(h):
+        for j in range(w):
+            if m[i][j] == 1:
+                bfs(i,j,w,h,m)
+                cnt+=1
+    print(cnt)
+    w, h = map(int, input().split())
 
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] == 1: 
-                bfs(j, i)
-                res += 1
-    result.append(res) 
-
-for i in result:
-    print(i)
